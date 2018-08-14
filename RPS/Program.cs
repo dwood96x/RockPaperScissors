a02b9d64655cfd55095ae9711ff458f95a6fcc4c
David Wood <dwood96x@gmail.com>
2018-08-14 08:53:42 -0400

    Add project files.

---
+++ RPS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                fight();
+            } while played == 0;
+        }
+        public static void fight()
+        {
+            Console.WriteLine("Lets play rock paper scissors! Whats your choice?");
+            string pChoice = Console.ReadLine();
+            pChoice.ToLower();
+            Random random = new Random();
+            int aiChoice = random.Next(2);
+
+            if pChoice /=
+
+        }
+    }
+}
