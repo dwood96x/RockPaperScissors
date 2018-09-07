59de75c755ebafcf7c66f11eba7d8f5bb0864d36
David Wood <dwood96x@gmail.com>
2018-09-07 11:06:25 -0400

    Player can quit program and do loop is inside fight method.

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -10,70 +10,74 @@ namespace RPS
     {
         static void Main(string[] args)
         {
-            int played = 0;
-            do
-            {
                 fight();
-            } while (played == 0);
-            
         }
         public static void fight()
         {
-            Console.WriteLine("Lets play rock paper scissors! Whats your choice?");
-            string pChoiceOrig = Console.ReadLine();
-            string pChoice = pChoiceOrig.ToLower();
-            Random random = new Random();
-            int aiChoice = random.Next(3);
-            // 0 == rock, 1 == paper, 2 == scissors for the AI decision
-            if (pChoice == "rock")
+            int quitGame = 0;
+            do
             {
-                if (aiChoice == 0)
+                Console.WriteLine("Lets play rock paper scissors! Whats your choice?");
+                Console.WriteLine("Rock Paper Scissors or quit to exit.");
+                string pChoiceOrig = Console.ReadLine();
+                string pChoice = pChoiceOrig.ToLower();
+                Random random = new Random();
+                int aiChoice = random.Next(3);
+                // 0 == rock, 1 == paper, 2 == scissors for the AI decision
+                if (pChoice == "rock")
                 {
-                    Console.WriteLine("Draw! You both picked rock!");
+                    if (aiChoice == 0)
+                    {
+                        Console.WriteLine("Draw! You both picked rock!");
+                    }
+                    else if (aiChoice == 1)
+                    {
+                        Console.WriteLine("You lost! Their paper beat your rock!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You won! Congradulations!");
+                    }
                 }
-                else if (aiChoice == 1)
+                else if (pChoice == "paper")
                 {
-                    Console.WriteLine("You lost! Their paper beat your rock!");
+                    if (aiChoice == 0)
+                    {
+                        Console.WriteLine("You won! Your paper beat their rock!");
+                    }
+                    else if (aiChoice == 1)
+                    {
+                        Console.WriteLine("Draw! You both picked paper!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You lost! Their scissors beat your rock!");
+                    }
+                }
+                else if (pChoice == "scissors")
+                {
+                    if (aiChoice == 0)
+                    {
+                        Console.WriteLine("You lost! Their rock beat your scissors!");
+                    }
+                    else if (aiChoice == 1)
+                    {
+                        Console.WriteLine("You win! Your scissors beat their paper!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Draw! You both picked scissors!");
+                    }
+                }
+                else if (pChoice == "quit")
+                {
+                    quitGame = 1;
                 }
                 else
                 {
-                    Console.WriteLine("You won! Congradulations!");
+                    Console.WriteLine("Invalid input!");
                 }
-            }
-            else if (pChoice == "paper")
-            {
-                if (aiChoice == 0)
-                {
-                    Console.WriteLine("You won! Your paper beat their rock!");
-                }
-                else if (aiChoice == 1)
-                {
-                    Console.WriteLine("Draw! You both picked paper!");
-                }
-                else
-                {
-                    Console.WriteLine("You lost! Their scissors beat your rock!");
-                }
-            }
-            else if (pChoice == "scissors")
-            {
-                if (aiChoice == 0)
-                {
-                    Console.WriteLine("You lost! Their rock beat your scissors!");
-                }
-                else if (aiChoice == 1)
-                {
-                    Console.WriteLine("You win! Your scissors beat their paper!");
-                }
-                else
-                {
-                    Console.WriteLine("Draw! You both picked scissors!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input!");
-            }
+            } while (quitGame == 0);
         }
         public static void menu()
         {
